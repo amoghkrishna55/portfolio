9db2ff3badfd40bea949d87d57e6513462efe89b
Amogh Krishna <amoghkrishna55@gmail.com>
2024-10-10 02:20:54 +0530

    add project data collection arr

---
+++ data/projects.js
@@ -0,0 +1,14 @@
+
+// contains the list of projects
+const projects = [
+    {
+        title: "Runx",
+        description:  "RunX is a Remote Code Execution Engine Written in JavaScript. Every code submission is run in isolated environments. Uses containers for security and isolation.",
+        tags: [ "NodeJs", "Docker", "Linux"],
+        demoUrl: "https://runx.alanj.live/api/runtimes",
+        githubUrl: "https://github.com/alanJames00/runx",
+        url: "https://runx.alanj.live"
+    }
+]
+
+export default projects;
